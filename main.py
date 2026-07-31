f45df0bcbbf0f89abf47ec7e5b051cfcfb59370c
Sradhanjaliguddu <sradhanjaliguddu601@gmail.com>
2026-07-31 20:57:46 +0530

    PYTHON BASIC PROJECT SNAKE -WATER -GUN

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
-import time
-from plyer import notification
-
-def water_reminder():
-	# """Send a desktop notification every interval_minutes reminding to drink water."""
-	# interval = max(1, int(interval_minutes)) * 60
-	
-	while True:
-			notification.notify(
-				title="Drink Water Sradha",
-				message="Time to drink water and stay hydrated!",
-				timeout=10
-			)
-			# time.sleep(3600)
-			time.sleep(3)
-	# except KeyboardInterrupt:
-	# 	print("Water reminder stopped.")
+import random
 
 
-# if __name__ == '__main__':
-	# default: remind every 60 minutes; change as needed
-	water_reminder()
+def game_win(user, computer):
+    if user == computer:
+        return None
+    
+    #Sanke vs water
+    if user == "s" and computer == "w":
+        return True
+    if user == "w" and computer == "s":
+        return False
+    
+    #Water vs Gun
+    if user == "w" and computer == "g":
+        return True
+    if user == "g" and computer == "w":
+        return False
+    
+    # Gun Vs Snake
+    if user == "g" and computer == "s":
+        return True
+    if user == "s" and computer == "g":
+        return False
+
+
+ran_no = random.randint(1, 3)
+
+
+
+print("computer's turn: snake(s), Water(w), Gun (g)")
+if ran_no == 1:
+    computer = "s"
+elif ran_no == 2:
+    computer = "w"
+else:
+    computer = "g"
+
+user = input("your's turn: snake(s), Water(w), Gun (g) ").lower()
+
+
+result = game_win(user, computer) #Returns true if you win, False for lose, none for draw
+print(f"\nYou chose: {user}")
+print(f"\nComputer chose: {computer}")
+
+if result is None:
+    print("Its a draw!")
+
+elif(result):
+    print("You win!")
+else:
+    print("You lose!")
